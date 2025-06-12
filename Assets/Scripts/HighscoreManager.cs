1a96d54a2ed1b6aeb7bad266a1c5eb83ea5ce2c7
WastedNS <ns@wastedstudio.com>
2025-06-12 20:47:15 +0200

    Data save fixed

--- Assets/Scripts/HighscoreManager.cs
+++ Assets/Scripts/HighscoreManager.cs
@@ -1,11 +1,14 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HighscoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	public TMP_InputField inputField;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
         
         
@@ -18,11 +21,24 @@ public class HighscoreManager : MonoBehaviour
     }
 
     public void SaveHighscore()
-    { 
-        var inputName = gameObject.GetComponent<InputField>().text;
-        MainManager.Instance.SaveData(inputName);
+    {
+		//var inputName = inputField.text;
+		// Get the input name from the InputField in the Canvas
+		//InputField inputField = GameObject.Find("InputField").GetComponent<InputField>();
+		//InputField inputField = GameObject.Find("Canvas").GetComponent<InputField>();
+		string inputName = inputField.text;
+		// Check if the input name is empty
+		if (string.IsNullOrEmpty(inputName))
+		{
+			Debug.LogWarning("Input name is empty. Please enter a valid name.");
+			return;
+		}
+
+		// Save the high score data using MainManager
+		MainManager.Instance.SaveData(inputName);
 
         // Load scene 0
         SceneManager.LoadScene(0);
+		MainManager.Instance.Init();
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -9,153 +9,164 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+	public static MainManager Instance;
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
 
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+	public Brick BrickPrefab;
+	public int LineCount = 6;
+	public Rigidbody Ball;
 
-    public Text ScoreText;
-    public Text HighScoreText;
-    public GameObject GameOverText;
+	public Text ScoreText;
+	public Text HighScoreText;
+	public GameObject GameOverText;
 
-    private bool m_Started = false;
-    private int m_Points;
-    private int m_HighScore;
+	private bool m_Started = false;
+	private int m_Points;
+	private int m_HighScore;
 
-    private bool m_GameOver = false;
+	private bool m_GameOver = false;
 
-    const string savefileName = "savefile.json";
+	const string savefileName = "savefile.json";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
+	// Start is called before the first frame update
+	void Start()
+	{
+		Init();
+	}
 
-        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
+	public void Init()
+	{
+		m_GameOver = false;
+		m_Started = false;
+		m_Points = 0;
+		ScoreText.text = $"Score: {m_Points}";
 
-        LoadData();
-    }
+		const float step = 0.6f;
+		int perLine = Mathf.FloorToInt(4.0f / step);
 
-    private void Update()
-    {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = UnityEngine.Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+		int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+		for (int i = 0; i < LineCount; ++i)
+		{
+			for (int x = 0; x < perLine; ++x)
+			{
+				Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+				var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+				brick.PointValue = pointCountArray[i];
+				brick.onDestroyed.AddListener(AddPoint);
+			}
+		}
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
+		LoadData();
+	}
 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+	private void Update()
+	{
+		if (!m_Started)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				m_Started = true;
+				float randomDirection = UnityEngine.Random.Range(-1.0f, 1.0f);
+				Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+				forceDir.Normalize();
 
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Screen.fullScreen = !Screen.fullScreen;
-        }
+				Ball.transform.SetParent(null);
+				Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+			}
+		}
+		else if (m_GameOver)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+				Init();
+			}
+		}
 
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            m_HighScore = 0;
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 
-    void AddPoint(int point)
-    {
-        m_Points += point;
-        ScoreText.text = $"Score: {m_Points}";
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			Screen.fullScreen = !Screen.fullScreen;
+		}
 
-        if (m_Points > m_HighScore)
-        {
-            //m_HighScore = m_Points;
-            HighScoreText.text = $"High Score: {m_HighScore}";
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			m_HighScore = 0;
+		}
+	}
 
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+	void AddPoint(int point)
+	{
+		m_Points += point;
+		ScoreText.text = $"Score: {m_Points}";
 
-        if (m_Points > m_HighScore)
-        {
-            m_HighScore = m_Points;
-            HighScoreText.text = $"High Score: {m_HighScore}";
+		if (m_Points > m_HighScore)
+		{
+			//m_HighScore = m_Points;
+			HighScoreText.text = $"New High Score: {m_HighScore}";
+		}
+	}
 
-            // Show the scene with the high score
-            SceneManager.LoadScene(1);
-        }
-    }
+	public void GameOver()
+	{
+		m_GameOver = true;
+		GameOverText.SetActive(true);
 
-    public void SaveData(string name)
-    {
-        SaveDataVm data = new()
-        {
-            HighScore = m_HighScore,
-            HighScoreText = $"{HighScoreText.text} {name}",
-        };
+		if (m_Points > m_HighScore)
+		{
+			m_HighScore = m_Points;
+			HighScoreText.text = $"New High Score: {m_HighScore}";
 
-        string json = JsonUtility.ToJson(data, true);
+			// Show the scene with the high score
+			SceneManager.LoadScene(1);
+		}
+	}
 
-        File.WriteAllText(Application.persistentDataPath + "/" + savefileName, json);
-    }
+	public void SaveData(string name)
+	{
+		SaveDataVm data = new()
+		{
+			HighScore = m_HighScore,
+			HighScoreText = $"{HighScoreText.text} {name}",
+		};
 
-    public void LoadData()
-    {
-        string path = Application.persistentDataPath + "/" + savefileName;
-        if (File.Exists(path))
-        {
-            string json = File.ReadAllText(path);
-            SaveDataVm data = JsonUtility.FromJson<SaveDataVm>(json);
+		string json = JsonUtility.ToJson(data, true);
 
-            m_HighScore = data.HighScore;
-            HighScoreText.text = data.HighScoreText;
-        }
-    }
+		File.WriteAllText(Application.persistentDataPath + "/" + savefileName, json);
+	}
 
-    [Serializable]
-    class SaveDataVm
-    {
-        public int HighScore;
-        public string HighScoreText;
-    }
+	public void LoadData()
+	{
+		string path = Application.persistentDataPath + "/" + savefileName;
+		if (File.Exists(path))
+		{
+			string json = File.ReadAllText(path);
+			SaveDataVm data = JsonUtility.FromJson<SaveDataVm>(json);
+
+			m_HighScore = data.HighScore;
+			HighScoreText.text = data.HighScoreText;
+		}
+	}
+
+	[Serializable]
+	class SaveDataVm
+	{
+		public int HighScore;
+		public string HighScoreText;
+	}
 }
